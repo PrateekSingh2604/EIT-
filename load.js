66fa598804faf74ea22613b70ed822d155f78f40
PrateekSingh2604 <prateek.art2002@gmail.com>
2024-04-16 23:56:45 +0530

    glance

--- load.js
+++ load.js
@@ -35,6 +35,13 @@ const alumni = document.querySelector(".eit-box .boxes .box2")
 const cplacement = document.querySelector(".eit-box .boxes .box3")
 const recruiters = document.querySelector(".eit-box .boxes .box4")
 
+const labs = document.querySelector(".glance-detail .glance-about .box2");
+const establishment = document.querySelector(".glance-detail .glance-about .box1");
+const books = document.querySelector(".glance-detail .glance-about .box3");
+const rooms = document.querySelector(".glance-detail .glance-about .box4");
+const print = document.querySelector(".glance-detail .glance-about .box5");
+const area = document.querySelector(".glance-detail .glance-about .box6");
+
 function number(element, max, text, value, plus) {
   const triggerBottom = window.innerHeight / 4*5;
 
@@ -42,7 +49,7 @@ function number(element, max, text, value, plus) {
 
 
   if (classes < triggerBottom) {
-    // element.style.border = "2px solid red";
+    console.log("hello")
     console.log(classes)
     let count = 0;
     const interval = setInterval(() => {
@@ -53,6 +60,12 @@ function number(element, max, text, value, plus) {
         window.removeEventListener('scroll', excellence_number);
         window.removeEventListener('scroll', campus_placement);
         window.removeEventListener('scroll', campus_recruiters);
+        window.removeEventListener('scroll', establishment_eit);
+        window.removeEventListener('scroll', labs_workshops);
+        window.removeEventListener('scroll', book);
+        window.removeEventListener('scroll', classrooms);
+        window.removeEventListener('scroll', national_print);
+        window.removeEventListener('scroll', eit_area);
         clearInterval(interval);
       }
     }, 15)
@@ -72,7 +85,30 @@ function campus_placement() {
 function campus_recruiters() {
   number(recruiters, 165, "CAMPUS RECRUITERS", 5, "+");
 }
-
+function labs_workshops(){
+  number(labs, 70, "Labs & Workshops", 2, "+")
+}
+function establishment_eit(){
+  number( establishment,  2007, "Establishment Year", 9, "")
+}
+function book(){
+  number(books,  40000, "Books", 200, "+")
+}
+function classrooms(){
+  number(rooms,  50, "Classrooms and Tutorial Rooms", 1, "+")
+}
+function national_print(){
+  number(print,  58, "National & International Print Journals", 1, "+")
+}
+function eit_area(){
+  number(area,  17.5, "Acres Green Lush Campus", 0.5, "+")
+}
+window.addEventListener("scroll", eit_area);
+window.addEventListener("scroll", national_print);
+window.addEventListener("scroll", classrooms);
+window.addEventListener("scroll", book);
+window.addEventListener("scroll", labs_workshops);
+window.addEventListener("scroll", establishment_eit);
 window.addEventListener("scroll", campus_recruiters);
 window.addEventListener("scroll", campus_placement);
 window.addEventListener("scroll", alumni_number);
